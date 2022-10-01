cd729d8592e6f7ef514f48d64d0a27d7a44ab1c2
Ishaan Gupta <ixg97@case.edu>
2022-10-01 11:05:39 -0400

    fixed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile('./index.html')
+  res.sendFile(path.join(__dirname+'/index.html'));
 })
 
 app.listen(port, () => {
