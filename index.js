967a16681cccab78b6fe9eaea8b510da25e6a2e9
Lucas Tilford <lmt95@case.edu>
2022-10-01 13:53:49 -0400

    load model

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const path = require('path');
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/model", (req, res) => {
+  res.sendFile(path.join(__dirname + "/model/iss.dae"));
+});
